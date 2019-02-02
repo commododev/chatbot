f5551ea4fdfdf501aac2256a9eeefa44c61f5673
tabata0330 <tabata@db.ics.keio.ac.jp>
2019-02-02 15:40:45 +0900

    revise

--- skill/start.js
+++ skill/start.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = class HandlePizzaOrder {
+module.exports = class start {
     // パラメーターが全部揃ったら実行する処理を記述します。
     async finish(bot, event, context){
         let message = {
