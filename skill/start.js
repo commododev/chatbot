caa83656a9a31d05011ce2978041e9b373e5bfd8
tabata0330 <tabata@db.ics.keio.ac.jp>
2019-02-02 18:09:40 +0900

    だよ~追加

--- skill/start.js
+++ skill/start.js
@@ -4,7 +4,7 @@ module.exports = class start {
     async finish(bot, event, context){
         let message = {
             type: "text",
-            text: `命の恩人`
+            text: `命の恩人だよ~`
         };
 
         await bot.reply(message);
