ce68a5a59392227a1778d60ebaf5d2c645c8e58b
Masafumi <musasino776@gmail.com>
2019-02-02 17:44:49 +0900

    start.jsを変更

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 */
 const server = require("express")();
 const bot_express = require("bot-express");
+const config = require("config");
 
 /*
 ** Middleware Configuration
@@ -20,25 +21,25 @@ server.use("/bot/webhook", bot_express({
     language: "ja",
     messenger: {
         line: {
-            channel_id: process.env.LINE_CHANNEL_ID,
-            channel_secret: process.env.LINE_CHANNEL_SECRET
+            channel_id: process.env.LINE_CHANNEL_ID || config.env.LINE_CHANNEL_ID,
+            channel_secret: process.env.LINE_CHANNEL_SECRET || config.env.LINE_CHANNEL_SECRET
         }
     },
     nlu: {
         type: "dialogflow",
         options: {
-            project_id: process.env.GOOGLE_PROJECT_ID,
-            client_email: process.env.GOOGLE_CLIENT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY,
+            project_id: process.env.GOOGLE_PROJECT_ID || config.env.GOOGLE_PROJECT_ID,
+            client_email: process.env.GOOGLE_CLIENT_EMAIL || config.env.GOOGLE_CLIENT_EMAIL,
+            private_key: process.env.GOOGLE_PRIVATE_KEY || config.env.GOOGLE_PRIVATE_KEY,
             language: "ja"
         }
     },
     parser: [{
         type: "dialogflow",
         options: {
-            project_id: process.env.GOOGLE_PROJECT_ID,
-            client_email: process.env.GOOGLE_CLIENT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY,
+            project_id: process.env.GOOGLE_PROJECT_ID || config.env.GOOGLE_PROJECT_ID,
+            client_email: process.env.GOOGLE_CLIENT_EMAIL || config.env.GOOGLE_CLIENT_EMAIL,
+            private_key: process.env.GOOGLE_PRIVATE_KEY || config.env.GOOGLE_PRIVATE_KEY,
             language: "ja"
         }
     }],
@@ -51,9 +52,9 @@ server.use("/bot/webhook", bot_express({
         enable_lang_detection: false,
         enable_translation: false,
         options: {
-            project_id: process.env.GOOGLE_PROJECT_ID,
-            client_email: process.env.GOOGLE_CLIENT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY
+            project_id: process.env.GOOGLE_PROJECT_ID || config.env.GOOGLE_PROJECT_ID,
+            client_email: process.env.GOOGLE_CLIENT_EMAIL || config.env.GOOGLE_CLIENT_EMAIL,
+            private_key: process.env.GOOGLE_PRIVATE_KEY || config.env.GOOGLE_PRIVATE_KEY
         }
     },
     skill: {
--- skill/start.js
+++ skill/start.js
@@ -4,7 +4,7 @@ module.exports = class start {
     async finish(bot, event, context){
         let message = {
             type: "text",
-            text: `こんにちは!!`
+            text: `命の恩人`
         };
 
         await bot.reply(message);
