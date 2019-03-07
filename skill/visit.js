9937d5be1da78abffe9c428ab1026e775599e35b
Masafumi <musasino776@gmail.com>
2019-03-08 02:33:31 +0900

    表現の統一「訪問演奏」

--- skill/visit.js
+++ skill/visit.js
@@ -1,4 +1,4 @@
-// このファイルは訪問演奏会について問い合わせられた場合のskillファイルです。
+// このファイルは訪問演奏について問い合わせられた場合のskillファイルです。
 // 1. 日程、必要楽器、拘束時間について
 // 2. 登録について
 // の二つに分岐させるボタンの作成をします。
@@ -9,7 +9,7 @@ module.exports = class visit {
     async finish(bot, event, context){
         let message = {
             type: "text",
-            text: "訪問演奏会についてお問い合わせありがとうございます。"
+            text: "訪問演奏についてお問い合わせありがとうございます。"
         };
 
         await bot.reply(message);
